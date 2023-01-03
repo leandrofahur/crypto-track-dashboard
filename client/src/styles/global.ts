74488298038d8b73bddcc3606ab9ebbedbcd0e51
leandro <fahurleandro@gmail.com>
2023-01-02 19:59:27 -0800

    eslint configuration from rocketseat

---
+++ client/src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background-color: #333;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button{
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
---
+++ client/src/styles/themes/light.ts
@@ -0,0 +1,13 @@
+export const light = {
+  white: '#fff',
+
+  'primary-900': '',
+  'primary-800': '',
+  'primary-700': '',
+  'primary-600': '',
+  'primary-500': '',
+  'primary-400': '',
+  'primary-300': '',
+  'primary-200': '',
+  'primary-100': '',
+}
